694dbdd753f98dced6c4470680908eea0cc2fd5e
Samuel Argueta <jarguetahernandez02@gmail.com>
2023-12-26 19:40:42 -0600

    Revalidation

--- cap06/app/page.tsx
+++ cap06/app/page.tsx
@@ -1,5 +1,7 @@
 import Posts from "./components/Posts";
 
+export const revalidate = 10
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
