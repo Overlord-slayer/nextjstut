752c8109f28867e69b6371ad5b17b646c650f725
Samuel Argueta <jarguetahernandez02@gmail.com>
2023-12-26 18:59:37 -0600

    Middleware

--- cap08/src/app/api/todos/route.ts
+++ cap08/src/app/api/todos/route.ts
@@ -7,12 +7,18 @@ const API_KEY: string = process.env.DATA_API_KEY as string
  * 
  * @returns 
  */
-export async function GET() {
+export async function GET(request: Request) {
+  const origin = request.headers.get('origin')
   const res = await fetch(DATA_SOURCE_URL)
 
   const todos: Todo[] = await res.json()
 
-  return NextResponse.json(todos)
+  return new NextResponse(JSON.stringify(todos), {
+    headers: {
+      'Access-Control-Allow-Origin': origin || "*",
+      'Content-Type': 'application/json'
+    }
+  })
 }
 
 /**
